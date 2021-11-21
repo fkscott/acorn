37c8f727a1542de1619d5ab735a4b330e8ace7cc
keagan <fkscott@loyola.edu>
2021-11-21 14:36:18 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Acorn Supply and Distribution
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/*!
+* Start Bootstrap - Scrolling Nav v5.0.4 (https://startbootstrap.com/template/scrolling-nav)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+
+});
